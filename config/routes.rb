e2c3446335bc54fbaad2c6470da1b0e6aec45ad2
brainconnect93 <brainconnect0@gmail.com>
2022-10-27 00:14:45 +0100

    Edit the devise folder for better view

--- config/routes.rb
+++ config/routes.rb
@@ -1,20 +1,22 @@
 Rails.application.routes.draw do
   devise_for :users
-  
-  authenticated :user do
-    root 'groups#index', as: :authenticated_root
-  end
+  root to: 'users#index'
+  # authenticated :user do
+  #   root 'groups#index', as: :authenticated_root
+  # end
 
-  unauthenticated :user do
-    root to: 'users#index'
-  end
-  devise_scope :user do
-    get '/users/sign_out' => 'devise/sessions#destroy'
-  end
+  # unauthenticated :user do
+  #   root to: 'users#index'
+  # end
+  # devise_scope :user do
+  #   get '/users/sign_out' => 'devise/sessions#destroy'
+  # end
+  
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
+  
   resources :users, only: [:index]
   resources :groups, only: %i[index show new create destroy] do
     resources :expenses, only: %i[index show new create destroy]
