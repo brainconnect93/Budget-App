6e2fcd71c07c5bb61519cbfa9c1e614cf1993291
brainconnect93 <brainconnect0@gmail.com>
2022-10-27 10:57:55 +0100

    Validates all the files in the models

--- app/models/expense.rb
+++ app/models/expense.rb
@@ -1,4 +1,7 @@
 class Expense
   belongs_to :user
   belongs_to :group
+
+  validates :name, presence: true, length: { maximum: 50 }
+  validates :amount, presence: true, numericality: { greater_than: 0 }
 end
