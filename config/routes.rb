53060bc77b859732cb0bb432ab4bd167b48761ca
brainconnect93 <brainconnect0@gmail.com>
2022-10-28 00:31:46 +0100

    Work with the route and finalize the devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,16 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to: 'users#index'
-  # authenticated :user do
-  #   root 'groups#index', as: :authenticated_root
-  # end
+  # root to: 'users#index'
+  authenticated :user do
+    root 'groups#index', as: :authenticated_root
+  end
 
-  # unauthenticated :user do
-  #   root to: 'users#index'
-  # end
-  # devise_scope :user do
-  #   get '/users/sign_out' => 'devise/sessions#destroy'
-  # end
+  unauthenticated :user do
+    root to: 'users#index'
+  end
+  devise_scope :user do
+    get '/users/sign_out' => 'devise/sessions#destroy'
+  end
   
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
